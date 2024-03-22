532fee5997bc5de3d3dc93ec73bab651aaeb8d64
zzinkyum <160145805+zzinkyum@users.noreply.github.com>
2024-03-22 17:49:22 +0900

    naming_convention.js

---
+++ twitter_markup_challenge/naming_convention.js
@@ -0,0 +1,8 @@
+/* 카멜 케이스 (camelCase) => 변수, 함수의 이름 */
+var firstName;
+
+/* 스네이크 케이스 (snake_case) => 띄어쓰기를 사용할 때 */
+var first_name;
+
+/* 파스칼 케이스 (PascalCase) => 생성자 함수, 클래스의 이름 */
+var FirstName;
